cc09361be5f433adac07a3743b07dd769da3d260
Matt Westcott <mattwestcott@users.noreply.github.com>
2022-01-28 12:53:54 +0900

    Delete fastlane directory

--- fastlane/Appfile.swift
+++
@@ -1,9 +0,0 @@
-var appIdentifier: String { return "jp.co.senkogrouphd.hapilk.development" } // The bundle identifier of your app
-var appleID: String { return "[[APPLE_ID]]" } // Your Apple email address
-
-var itcTeam: String? { return "119703611" } // App Store Connect Team ID
-var teamID: String { return "P625W44XMJ" } // Apple Developer Portal Team ID
-
-
-// For more information about the Appfile, see:
-//     https://docs.fastlane.tools/advanced/#appfile
